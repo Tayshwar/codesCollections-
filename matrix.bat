0f466bc0f6501e23fa84c03c7ddacd98b157227d
johnmelodyme <44691957+johnmelodyme@users.noreply.github.com>
2018-11-02 16:10:53 +0800

    Matrix_an_hacker_file.bat

---
+++ matrix.bat
@@ -0,0 +1,5 @@
+color 2 
+@echo off
+:start
+echo %random% %random% 0100 011000010 00010001000 10 %random% %random% %random% %random% 01001011000 101001 0010010 %random% %random% %random% %random%
+goto start
